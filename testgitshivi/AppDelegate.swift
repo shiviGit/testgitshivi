3b94bab8b9f73f2a76b0d79171bd08e08fffcaf9
Apple <apple@Apples-MacBook-Pro.local>
2019-03-12 15:49:44 +0530

    Initial commit

--- testgitshivi/AppDelegate.swift
+++ testgitshivi/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-var string = ""
+var string = "xrsestdgdg"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
